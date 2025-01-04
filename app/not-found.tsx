7032d44cb5438626b111aa3e56ae4b93311d3f31
Sarfaraz Unar <sarfarazunarr@gmail.com>
2025-01-04 16:42:46 +0500

    notfound updated

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,7 +8,7 @@ const NotFound = () => {
                 <div className="text-center">
                     <h1 className="text-9xl font-bold">404</h1>
                     <p className="text-2xl font-medium mt-4">Oops! Page not found</p>
-                    <p className="mt-4 mb-8">The page you're looking for doesn't exist or has been moved.</p>
+                    <p className="mt-4 mb-8">The page you&apos;re looking for doesn&apos;t exist or has been moved.</p>
                     <Link href="/"
                         className="px-6 py-3 bg-white font-semibold rounded-full hover:bg-purple-100 transition duration-300 ease-in-out dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-white">
                         Go Back Home
